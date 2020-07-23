1a20ab02ae2b056b1bf0d917a8cc88c6814243e2
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2020-07-23 22:56:26 +0300

    Update FamilyTree.hpp

--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -3,72 +3,48 @@
 using namespace std;
 
 namespace family {
-class Node
-{
+    
+class Node {
+    
     string name;
     Node *father;
     Node *mother;
     Node *son;
 
 public:
+    // CONSTRUCTOR
     Node(const string& name);
 
-    const string& getName() const {
-        return this->name;
-    }
-
-    void setName(const string& name) {
-        this->name=name;
-    }
-
-    Node* getMother() const {
-        return this->mother;
-    }
-
-    void setMother(Node* mother) {
-        this->mother = mother;
-    }
-
-    void setFather(Node* father) {
-        this->father = father;
-    }
-
-    void setSon(Node* son) {
-        this->son = son;
-    }
-
-
-    Node* getSon() const {
-        return this->son;
-    }
-
-    Node* getFather() const {
-        return this->father;
-    }
-
+    // getters
+    const string& getName() const {return this->name;}
+    Node* getMother() const {return this->mother;}
+    Node* getSon() const {return this->son;}
+    Node* getFather() const {return this->father;}
+    
+    // setters
+    void setName(const string& name) {this->name=name;}
+    void setMother(Node* mother) {this->mother = mother;}
+    void setFather(Node* father) {this->father = father;}
+    void setSon(Node* son) {this->son = son;}
 };
 
 
     class Tree {
+       
         Node *root;
 
         Node* findTheSon(const string&, Node*) const;
         void addParent(const string& parent, const string& son, bool addToFather);
         void printSubtree(Node* root, const string& prefix);
 
-    public:
+  
+        public:
         Tree(const string&);
-
         Tree& addFather(const string&, const string&);
-
         Tree& addMother(const string&, const string&);
-
         const string relation(const string& name);
-
         const string& find(const string&);
-
         void display();
-
         void remove(const string&);
     };
 }

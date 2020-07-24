edc2ef8e0a0783c177446e709a5bc58b5b572839
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2020-07-24 13:09:32 +0300

    Update FamilyTree.hpp

--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -27,7 +27,7 @@ namespace family{
 		Tree& addFather(string son, string father); // Adding a father to someone who already exists at the Tree
 		Tree& addMother(string son, string father);  // Adding a mother to someone who already exists at the Tree
 		void display(); // print the Tree
-		void remove(string toRemove); // Delete all sub-tree of the name we got
+		void remove(string name); // Delete all sub-tree of the name we got
 		string find(string toSearch); // returns the name that feet to the relation we got  
 		string relation(string rfamilyRelation); // returns the relation between me and the name
 		
